2ecdb8c645706958c949d8b988930eb1ad26d09c
Drăghici Andrei <draghici.andrei12@yahoo.com>
2025-11-28 15:16:18 +0200

    Structural updates

--- main.py
+++ main.py
@@ -6,9 +6,37 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/left_sidebar")
-def left_sidebar():
-    return render_template("left-sidebar.html")
+# @app.route("/left_sidebar")
+# def left_sidebar():
+#     return render_template("left-sidebar.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/studies")
+def studies():
+    return render_template("studies.html")
+
+@app.route("/career")
+def career():
+    return render_template("career.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/blog/day_1")
+def day_1():
+    return render_template("day_1.html")
+
+@app.route("/blog/greetings")
+def greetings():
+    return render_template("greetings.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
 
 @app.route("/no_sidebar")
 def no_sidebar():
